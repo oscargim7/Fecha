0fcbfbfb2e0ee8a9962ef5d44a750e1f09e32586
oscargim7 <oscarromero1601@gmail.com>
2022-09-20 19:57:22 -0500

    Correciones

--- Date.cpp
+++ Date.cpp
@@ -1,57 +1,77 @@
 #include<iostream>
-//using namespace std;->Te incluye las funciones standard (std)
+#include<sstream>
 
+//using namespace std;->Te incluye las funciones standard (std)
 class Date
 {
     private:
-        int day, month, year;
-        std::string fecha; 
+        int day, month, year; 
 
-    void add_day(int nDayAdd)
-    {
-        day += nDayAdd;
-        if(day > 30)
+    public:   
+        void add_day(int nDayAdd)
         {
-            //add_month(day / 30);
-            day %= 30;
-        }
+            if(day > 31)
+            {
+                std::cout<<"El dia es invalido";
+            }
+            else
+            {
+                std::cout<<"El dia es valido";
+            }
 
-    };
+        };
 
-    void add_month()
-    {
-
-    };
+        void add_month()
+        {
+            if(month > 12)
+            {
+                std::cout<<"El mes sobrepasa el limite de meses";
+            }
+            else if(month <= 0)
+            {
+                std::cout<<"El mes esta debajo del limite de meses";
+            }
+        };
 
     public:
-
-    /*
-
-        Date(/* args );*/
-        //~Date();
+        //Llamada a métodos de constructor y destructor
+        Date( );
+        ~Date();
 };
 
+int main ()
+{   
+    //Se instancia el objeto
+    Date a;
+
+    int _day, _month, _year;
+    std::string fecha;
+    char _delimitador;
+
+    _delimitador = '/';
+    
+    std::cout<<"Ingresa la fecha del dia de hoy en formato aaaa/mm/dd";
+    std::cin>>fecha;
+
+    //Se separa la fecha ingresada por el usuario en 3 partes (dia, mes y año) por medio del delimitador "/"
+    _delimitador = std::stoi(fecha);      
+
+    std::cout<<"El dia ingresado es: ", _day;
+    std::cout<<"El mes ingresado es: ", _month;
+    std::cout<<"El anio ingresado es: ", _year;
+
+    return 0;
+}
+
+ //Constructor
     Date::Date(/* args */)
     {
         
     }
 
+    //Destructor
     Date::~Date()
     {
         
     }
-
-int main ()
-{
-    Date a;
-
-    std::string fecha;
-
-    std::cout<<"Ingresa la fecha del dia de hoy";
-    std::cin>>fecha;
-
-    
-
-    return 0;
-}
 
