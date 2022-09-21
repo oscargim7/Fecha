2e62baa4dc81233c8247e4d8dbceb17218f4c3df
oscargim7 <oscarromero1601@gmail.com>
2022-09-20 19:12:55 -0500

    Encapsulación de código

--- Date.cpp
+++ Date.cpp
@@ -1,7 +1,45 @@
 #include<iostream>
-#include<Date.h>
-
 //using namespace std;->Te incluye las funciones standard (std)
+
+class Date
+{
+    private:
+        int day, month, year;
+        std::string fecha; 
+
+    void add_day(int nDayAdd)
+    {
+        day += nDayAdd;
+        if(day > 30)
+        {
+            //add_month(day / 30);
+            day %= 30;
+        }
+
+    };
+
+    void add_month()
+    {
+
+    };
+
+    public:
+
+    /*
+
+        Date(/* args );*/
+        //~Date();
+};
+
+    Date::Date(/* args */)
+    {
+        
+    }
+
+    Date::~Date()
+    {
+        
+    }
 
 int main ()
 {
